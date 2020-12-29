6029b1dd348dcd03e1b35b880659d52a09713c41
balajibathe <balaji.bathe@meghaz.com>
2020-12-29 23:09:19 +0530

    Updates

---
+++ precache-manifest.e0b5341e5e360dca272b28b8a2667c68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c00ea0228572fce7689a93d9de9ac94",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2c9f7fa06425e4e50d7e",
+    "url": "./static/css/main.5fcc1fc0.chunk.css"
+  },
+  {
+    "revision": "803890a4b24ed9f90eba",
+    "url": "./static/js/2.dad8443c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.dad8443c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c9f7fa06425e4e50d7e",
+    "url": "./static/js/main.42f90f5d.chunk.js"
+  },
+  {
+    "revision": "7b5e0f72e0a5eec03b86",
+    "url": "./static/js/runtime-main.1bb82d29.js"
+  }
+]);
